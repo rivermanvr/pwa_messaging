9b897262b1e80152a163f18e639efe8a4b44e255
rivermanvr <vrgithub@riversconsulting.com>
2018-09-01 15:35:27 -0400

    Example 1 is complete

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ if (navigator.serviceWorker) {
 
   navigator.serviceWorker.register('/sw.js')
     .then(function(registration){
-
       console.log('main.js - SW Registered');
+
+      if(registration.active) {
+        registration.active.postMessage('Hi.  This is a message generated from main.js');
+      }
 
     }).catch(console.log);
 }
--- sw.js
+++ sw.js
@@ -4,3 +4,8 @@
 self.addEventListener('activate', () => {
   console.log('sw.js - SW: Active');
 });
+
+
+self.addEventListener('message', (e) => {
+  console.log(e.data);
+});
