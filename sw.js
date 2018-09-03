98a6bc634f2d0b1ad22a6fc6361e32143b1a8def
rivermanvr <vrgithub@riversconsulting.com>
2018-09-03 14:06:00 -0400

    setting up for example 3

--- sw.js
+++ sw.js
@@ -14,9 +14,17 @@ self.addEventListener('activate', () => {
 
 // Example 2.  re-comment lines 11-13  and un-comment the following lines:
 
+// self.addEventListener('message', (e) => {
+//     if(e.data === 'update_self') {
+//       console.log("The service worker is updating");
+//       self.skipWaiting();
+//     };
+// });
+
+//Example 3.  Re-comment lines 17-22 and un-comment the following lines:
+
 self.addEventListener('message', (e) => {
-    if(e.data === 'update_self') {
-      console.log("The service worker is updating");
-      self.skipWaiting();
-    };
-  });
+
+
+  
+});
