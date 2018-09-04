99215aa48573354f36608e7c5cb59a26e27fb8e9
rivermanvr <vrgithub@riversconsulting.com>
2018-09-03 22:19:02 -0400

    completed Example 5 Step 2

--- sw.js
+++ sw.js
@@ -23,14 +23,49 @@ self.addEventListener('activate', () => {
 
 //Example 3.  Re-comment lines 17-22 and un-comment the following lines:
 
-self.addEventListener('message', (e) => {
+// self.addEventListener('message', (e) => {
 
-  // Respond to all clients of a service worker.
-  self.clients.matchAll()
-    .then(clients => {
-      clients.forEach(client => {
-        client.postMessage("Hi.  This is a message from the service worker to all clients.");
-      })
-    })
+//   // Respond to all clients of a service worker.
+//   self.clients.matchAll()
+//     .then(clients => {
+//       clients.forEach(client => {
+//         client.postMessage("Hi.  This is a message from the service worker to all clients.");
+//       })
+//     })
+
+// });
+
+//Example 4 (Step 1) Re-comment lines 26-36 above. and un-comment the following lines:
+
+// self.addEventListener('message', (e) => {
+
+//   console.log('calling client: ', e);
+
+//   // Respond to all clients of a service worker.
+//   self.clients.matchAll()
+//     .then(clients => {
+
+//       console.log(clients)
+
+//       clients.forEach(client => {
+//         client.postMessage("Hi.  This is a message from the service worker to all clients.");
+//       })
+//     })
+
+//Example 5 (Step 2)  RE-comment out lines 40-53 and un-comment out the following:
+
+// self.addEventListener('message', (e) => {
+
+//   // Respond to all clients of a service worker.
+//   self.clients.matchAll()
+//     .then(clients => {
+
+//       clients.forEach(client => {
+
+//         if(e.source.id === client.id) {
+//           client.postMessage("Hi.  This is a message from the service worker to all clients.");
+//         }    
+//       })
+//     })
 
 });
